821607495c71fbb139437e769ac1515979c4c407
LO Olagoke <adderbyte@users.noreply.github.com>
2019-04-02 22:35:40 +0200

    Delete __init__.py

--- gym_xplane_final_version/xplane_gym/__init__.py
+++
@@ -1,7 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='gymXplane-v2',
-    entry_point='gym_xplane.envs:XplaneEnv',
-    kwargs={'clientAddr': '0.0.0.0', 'xpHost':'127.0.0.1', 'xpPort':49009, 'clientPort':1, 'timeout':3000, 'max_episode_steps':2000,'test':False}
-)
