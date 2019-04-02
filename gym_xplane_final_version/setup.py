e558d72446c7d8c2da4acc75a631b971fad2d586
LO Olagoke <adderbyte@users.noreply.github.com>
2019-04-02 22:27:39 +0200

    Add files via upload

---
+++ gym_xplane_final_version/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gym_xplane',
+      version='0.0.1',
+      description='Xplane environment for OpenAI Gym',
+      license='MIT License',
+      install_requires=['gym>=0.2.3'])
